20ad506bc851d98fd7c53526aab0db7a6d1d1de0
rich <rich.leblanc1@outlook.com>
2018-04-26 17:52:53 -0700

    x + 1

--- publisher.py
+++ publisher.py
@@ -11,11 +11,11 @@ channel = connection.channel() # start a channel
 channel.queue_declare(queue='pdfprocess') # Declare a queue
 # send a message
 
-num_msgs_to_send = 100
+num_msgs_to_send = 3
 
 for x in range(num_msgs_to_send):
-    print("Sending message {}/{}".format(x, num_msgs_to_send))
-    channel.basic_publish(exchange='', routing_key='pdfprocess', body="User information {}/{}".format(x, num_msgs_to_send))
+    print("Sending message {}/{}".format(x + 1, num_msgs_to_send))
+    channel.basic_publish(exchange='', routing_key='pdfprocess', body="User information {}/{}".format(x + 1, num_msgs_to_send))
     time.sleep(5) # delays for 5 seconds
     
 print ("[{}] Messages sent to consumer".format(num_msgs_to_send))
